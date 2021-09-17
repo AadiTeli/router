778fe5255fc4388df14d39d6b296a8d4e113d8c1
aaditeli <44204588+AadiTeli@users.noreply.github.com>
2021-09-17 14:34:12 +0530

    17 sep
    17 sep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./component/pages/Home";
+import About from "./component/pages/About";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <>
+    <BrowserRouter>
+      <Route exact path="/" component={Home} > </Route>
+      <Route exact path="/About" component={About} > </Route>
+      </BrowserRouter>
+    </>
+
+  )}
---
+++ src/router.jsx
@@ -0,0 +1,23 @@
+// import React from "react";
+// import {
+//   Switch,
+//   Route
+// } from "react-router-dom";
+// import Home from './component/pages/Home'
+// import About from './component/pages/About'
+// function App() {
+
+//   return (
+//     <div>
+//       <Switch>
+//         <Route exact path="/">
+//           <Home />
+//         </Route>
+//         <Route exact path="/about">
+//           <About />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+// export default App
